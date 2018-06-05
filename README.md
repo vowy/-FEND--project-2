22320b54dc034506d6392875dc737e98de4884c4
vowy <e.b.parsons@gmail.com>
2018-06-05 00:46:24 -0500

    Version 2.0! Fixed major clicking function bugs, added star rating, and new CSS styles update!

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # [FEND] Project 2: Memory/Matching Game Project
+[GitHub Pages Link](https://vowy.github.io/FEND-project-2/)
 
 ## Table of Contents
 
@@ -13,12 +14,18 @@ Made by, Eric "Vowy" Parsons along with help from the provided template.
 
 ## Instructions
 
-Click one the dark spaces to reveal a card with a symbol. Match cards by revealing two cards at a time. See move counter to track how many reveals you have initiated.
+Click one the dark spaces to reveal a card with a symbol. Match cards by revealing two cards at a time. See move counter to track how many it takes to complete. The star rating based on timer and move counter. Enjoy!
 
 ## Version
+Current: 2.0
+###BIG UPDATE!!
+-Fixed a lot of major bugs in the clicking functions
+-Added Star Rating
+-Updated end game/win modal
+-Snazzy updated CSS Styles (text-shadows!)
 
-Current: 1.0
+Previous (6/5/18): 1.0
 -Functionality mostly completed, next stop review!
 
-Previous(6/4/18):0.5 (BETA)
+Previous (6/4/18): 0.5 (BETA)
 (Note: Project is still in BETA, still a work in progress. Though it is mostly functional.)
--- js/app.js
+++ js/app.js
@@ -2,21 +2,54 @@
 const cards = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb', 'diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb'];
 
    const deck = document.querySelector('.deck');
+   const itemCard = deck.querySelector('li.card');
    const scorePanel = document.querySelector('.score-panel');
    const restart = scorePanel.querySelector('.restart');
    const moves = scorePanel.querySelector('.moves');
    const modal = document.querySelector('.modal');
-   let cardItem = [];
+   const stars = scorePanel.querySelector('.stars');
+   const starsLi = stars.querySelectorAll('.stars Li');
    let opened = [];
    let matchVar = 0;
    let movesVar = 0;
+   let starsVar = 5;
 
 // Score-panel restart (reload) function
-restart.addEventListener('click', function (){
+restart.addEventListener('click', function restartFunc (){
   location.reload(true);
 });
 
-
+//Star scoring system
+const starTemp = `<li class="stars Li"><i class = "fa fa-star"></i></li>`.repeat(starsVar)
+function makeStars () {
+stars.insertAdjacentHTML('beforeend', starTemp);
+}
+if (movesVar < 19) {
+setInterval (function(){
+  stars.removeChild(stars.lastElementChild);
+},30000);
+};
+if (movesVar > 18) {
+setInterval (function(){
+  starsVar--;
+  stars.removeChild(stars.lastElementChild);
+},20000);
+};
+if (movesVar > 25) {
+setInterval (function(){
+  starsVar--;
+  stars.removeChild(stars.lastElementChild);
+},10000);
+};
+if (movesVar > 30) {
+setInterval (function(){
+  starsVar--;
+  stars.removeChild(stars.lastElementChild);
+},1500);
+};
+if (starsVar < 0) {
+ let starsVar = 0;
+}
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -41,54 +74,30 @@ function placeCards() {
   };
 };
 
-//Card Clicking Functions
-function openCard (card) {
-  card.target.classList.add('open','show');
-  opened.push(card.target);
-}
-
-function closeCard (card) {
-  opened[1].classList.remove('open','show','unmatch');
-  opened[0].classList.remove('open','show','unmatch');
-  opened.splice(0,2);
-
-}
-
-//check card functionality
-   function checkCard(card)
-   {
-     if (opened.length > 1 && opened[0].firstChild.className === opened[1].firstChild.className) {
-       matchCards(card);
-       movesVar ++;
-       moves.innerText = movesVar;
-     } else {
-       movesVar ++;
-       moves.innerText = movesVar;
-       unmatchCards(card);
-       setTimeout(function(){
-       closeCard(card);
-     }, 600)
-     }
-   };
-
-//Matching cards functionality
- function matchCards (card){
-matchVar++;
-opened[0].classList.toggle('match');
-opened[1].classList.toggle('match');
-opened.splice(0,2);
- };
-
- function unmatchCards (card) {
-opened[0].classList.toggle('unmatch');
-opened[1].classList.toggle('unmatch');
- }
-
 //Function on Win
 function youWin(){
-  stopTimer();
-  modal.className = "modal-show";
+  deck.style.display = "none";
+  scorePanel.style.display = "none";
+if (movesVar === 16) {
+  var modalTemp = `<h1>PERFECT SCORE!<br>B E N N Y</h1> `.repeat(29)
+}
+if (min >= 0) {
+  var modalTemp = `<br><h3>You completed with a score of ${starsVar} out of 5 stars!<br>
+  You did it in ${movesVar} moves and ${sec} seconds!</h3><br>`;
 
+}
+if (min === 1) {
+  var modalTemp = `<br><h3>You completed with a score of ${starsVar} out of 5 stars!<br>
+  You did it in ${movesVar} moves and finished at ${min} minute and ${sec} seconds!</h3><br>`;
+
+}else{
+  var modalTemp = `<br><h3>You completed with a score of ${starsVar} out of 5 stars!<br>
+  You did it in ${movesVar} moves and finished at ${min} minutes and ${sec} seconds!</h3><br>`;
+}
+
+  modal.insertAdjacentHTML('beforeend', modalTemp);
+  modal.className=('modalshow');
+  stopTimer();
 }
 
 //timer
@@ -122,28 +131,76 @@ function insertTime() {
 	// display time
 	document.querySelector('.timer').innerHTML = "0" + min + ":" + sec;
 }
+//Card Clicking Functions
+function openCard (card) {
+  card.target.classList.add('open','show');
+  opened.push(card.target);
+}
+
+function closeCard (card) {
+  opened[1].classList.remove('open','show','unmatch');
+  opened[0].classList.remove('open','show','unmatch');
+  opened.splice(0,2);
+
+}
+
+//check card functionality
+   function checkCard(card)
+   {
+     if (opened.length > 1 && opened[0].firstChild.className === opened[1].firstChild.className && !(opened[0] === opened[1])) {
+       matchCards(card);
+       movesVar ++;
+       moves.innerText = movesVar;
+     } else {
+       movesVar ++;
+       moves.innerText = movesVar;
+       unmatchCards(card);
+       setTimeout(function(){
+       closeCard(card);
+     }, 600)
+     }
+   };
+
+//Matching cards functionality
+ function matchCards (card){
+opened[0].classList.toggle('match');
+opened[1].classList.toggle('match');
+matchVar++;
+opened.splice(0,2);
+
+
+
+ };
+
+ function unmatchCards (card) {
+opened[0].classList.toggle('unmatch');
+opened[1].classList.toggle('unmatch');
+ }
 
 //Initialize game function
  function gameInit(){
    startTimer();
    placeCards();
-   cardItem.push(deck.getElementsByTagName('i'));
-  deck.addEventListener('click', function (card) {
-      if (opened.length == 1) {
-         openCard(card);
-         checkCard(card);
-      }
-      else if (opened.length > 1) {
+   makeStars();
+//Clicking functions
+deck.addEventListener('click', function (card) {
 
-      } else if (opened.length == 0) {
-        openCard(card);
-       }
+        if (opened.length == 1 && card.target.classList.contains('card')) {
+           openCard(card);
+           checkCard(card);
+        }
+        else if (opened.length > 1) {
 
-        if (matchVar === 8) {
-            youWin();
+        } else if (opened.length == 0 && card.target.classList.contains('card')) {
+          openCard(card);
+         }
+          if (matchVar === 8) {
+              youWin();
+        }
       }
-    }
-  )
-}
+    )
+  };
+
+
 
 gameInit();
